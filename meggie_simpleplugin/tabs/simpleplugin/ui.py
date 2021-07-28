d04b62e22b07dffe8e341ee1a69c0dbbe21a5515
Erkka Heinila <erkkahe@gmail.com>
2021-07-28 14:52:17 +0300

    Update simpleplugin to work with meggie==1.2.0

--- meggie_simpleplugin/tabs/simpleplugin/ui.py
+++
@@ -1,8 +0,0 @@
-from meggie.utilities.messaging import messagebox
-
-
-def hello(experiment, data, window):
-    """ Helloes the active subject.
-    """
-    message = 'Hello {}!'.format(experiment.active_subject.name)
-    messagebox(window, message)
